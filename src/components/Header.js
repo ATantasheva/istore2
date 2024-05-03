cf290f3c883f83a0e240d137d6390fa708e1bdb9
ATantasheva <a4ypaxoid@yandex.ru>
2024-05-04 00:10:08 +0300

    header style

--- src/components/Header.js
+++
@@ -1,5 +0,0 @@
-function Header() {
-   return <header className='header'>header</header>;
-}
-
-export default Header;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import './style.css';
+
+function Header() {
+   return (
+   <header className='header'>
+      <div className='container'>
+     <div className="logo">istore</div>
+     </div>
+   </header>);
+}
+
+export default Header;
